d4012df3e98f9759ff4921859516b78a8525ce9e
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-10-27 13:54:12 +0600

    'login-added'

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaGoogle, FaGithub } from 'react-icons/fa';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 
 const Login = () => {
+    const { login } = useContext(AuthContext);
+
 
     const handelSubmit = e => {
         e.preventDefault()
@@ -13,19 +17,26 @@ const Login = () => {
         const password = form.password.value;
 
         console.log(email, password)
+
+        login(email, password)
+        .then(res => {
+            const user = res.user;
+            console.log(user)
+        })
+        .catch(error => console.error(error))
     }
 
     return (
-        <div className='mt-5'> 
+        <div className='mt-5'>
             <Form onSubmit={handelSubmit} className='mt-5'>
                 <section className="vh-90" >
                     <div className="container py-0 h-90">
                         <div className="row d-flex justify-content-center align-items-center h-90">
                             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                                <div className="card shadow-2-strong" style={{borderRadius: '1rem'}}>
+                                <div className="card shadow-2-strong" style={{ borderRadius: '1rem' }}>
                                     <div className="card-body p-5 text-centerj">
 
-                                        <h2 className="mb-5 text-center">Login</h2>
+                                        <h2 className="mb-5 text-center text-success">Login</h2>
 
                                         <Form.Group className="mb-3" controlId="formBasicEmail">
                                             <Form.Label>Email address</Form.Label>
@@ -39,15 +50,19 @@ const Login = () => {
 
 
                                         <div className='text-center'>
-                                            <Button variant='success' type='submit' className='mb-3 px-3'>Login</Button>
+                                            <Button variant='success' type='submit' className='mb-2 px-3'>Login</Button>
                                         </div>
-                                        <div>
+                                        <div className='d-flex justify-content-center py-1'>
+                                            <h3 className='me-3'><FaGoogle /></h3>
+                                            <h3><FaGithub /></h3>
+                                        </div>
+                                        <div className='text-center'>
                                             <p>Don't have an account? <Link to='/register'>Please register</Link></p>
                                         </div>
 
                                         {/* <hr className="my-4"> */}
 
-                                            {/* <button className="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
+                                        {/* <button className="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
                                                 type="submit"><i className="fab fa-google me-2"></i> Sign in with google</button>
                                             <button className="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;"
                                                 type="submit"><i className="fab fa-facebook-f me-2"></i>Sign in with facebook</button> */}
